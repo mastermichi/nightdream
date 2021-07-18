9e941bace294c627a4e9e6c8da116bb39f457fde
mastermichi <68911754+mastermichi@users.noreply.github.com>
2021-07-18 12:25:08 +0200

    1.1 bugfixes

--- src/com/firebirdberlin/nightdream/ui/RadioStreamDialogCustomAdapter.java
+++ src/com/firebirdberlin/nightdream/ui/RadioStreamDialogCustomAdapter.java
@@ -1,27 +1,21 @@
 package com.firebirdberlin.nightdream.ui;
 
-import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
-import com.android.volley.toolbox.Volley;
 import com.firebirdberlin.nightdream.R;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import androidx.collection.LruCache;
-
 import java.util.List;
 import java.util.Locale;
 
 public class RadioStreamDialogCustomAdapter extends ArrayAdapter<RadioStreamDialogItem> {
 
-    RequestQueue mRequestQueue;
     ImageLoader mImageLoader;
     Context context;
 
@@ -29,16 +23,7 @@ public class RadioStreamDialogCustomAdapter extends ArrayAdapter<RadioStreamDial
                                  List<RadioStreamDialogItem> items) {
         super(context, resourceId, items);
         this.context = context;
-        mRequestQueue = Volley.newRequestQueue(context);
-        mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
-            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
-            public void putBitmap(String url, Bitmap bitmap) {
-                mCache.put(url, bitmap);
-            }
-            public Bitmap getBitmap(String url) {
-                return mCache.get(url);
-            }
-        });
+        mImageLoader = VolleySingleton.getInstance().getImageLoader();
     }
 
     private class ViewHolder {
@@ -67,6 +52,8 @@ public class RadioStreamDialogCustomAdapter extends ArrayAdapter<RadioStreamDial
         holder.txtDesc.setText(getDesc(item));
 
        if (item.getImageUrl() != null && !item.getImageUrl().isEmpty()) {
+
+
            holder.imageView.setImageUrl(item.getImageUrl(), mImageLoader);
         }
 
